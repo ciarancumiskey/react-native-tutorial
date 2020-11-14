d787ef296e9c3d20b888fc148124336e27335925
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2020-11-14 16:31:13 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>
+        Hello there.
+      </Text>
+      <Text>
+        General Kenobi! You are a bold one!
+      </Text>
+    </View>
+  );
+}
+
+export default YourApp;
